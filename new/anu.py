64b22464daa35c1f51286223bde7ec95c8502c9c
Aviral Srivastava <aviralji4@gmail.com>
2021-09-06 13:12:21 +0530

    "updated the code base..."

--- new/anu.py
+++ new/anu.py
@@ -1,16 +1,4 @@
 # for random quick python questions that my love asks
-def sol(m, n) :
-    for k in range(m, n+1): 
-        i = 1 << 31
-        while(i > 0) :
-            if((k & i) != 0) :
-                print("1", end = "")
-            else :
-                print("0", end = "")
-            i = i // 2
-        print()
-             
-m = int(input())
-n = int(input())
-sol(m,n)
-print()
+
+anu = "anu"
+print("output - ", anu.capitalize())
