7ceca48ada2d0e97f078baf30eb94214abf6c972
Aviral Srivastava <aviralji4@gmail.com>
2021-09-28 23:08:01 +0530

    "updated the code base..."

--- new/amodqr.py
+++ new/amodqr.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+import qrcode
+from PIL import Image
+from qrcode.image.pil import PilImage
+import cv2
+from pyzbar.pyzbar import decode
 
 def create_qr():
-    import qrcode
-    from PIL import Image
-    from qrcode.image.pil import PilImage
     k=entry1.get()
     img=qrcode.make(k)
     img.save("q.jpg")
@@ -13,16 +15,12 @@ def create_qr():
     p.show()
 
 def read_img():
-    import cv2
-    from pyzbar.pyzbar import decode
     file_path = filedialog.askopenfilename()
     img1=cv2.imread(file_path)
     for code in decode(img1):
        label3.config(text = "Output: " + code.data.decode('utf-8'))
 
 def read_cam():
-    import cv2
-    from pyzbar.pyzbar import decode
     cap=cv2.VideoCapture(0)
     i=0
     while i<1:
@@ -32,14 +30,14 @@ def read_cam():
           i=i+1
        cv2.imshow("Screen",frame)
        cv2.waitKey(5)
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
 
 window = tk.Tk()
-window.geometry('400x300')
+window.geometry('700x300')
 greeting = tk.Label(text="Welcome to the QR Code Program")
 label1 = tk.Label(text="Enter Text to generate code")
 entry1 = tk.Entry()
-button1 = tk.Button(text="Generate QR Code", command=create_qr)
+button1 = tk.Button(text="Generate QR", command=create_qr)
 label2 = tk.Label(text="Select QR Image File to read:")
 button2 = tk.Button(text="Select Image", command=read_img)
 label3 = tk.Label(text="Output: ")
