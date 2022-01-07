0a05d8e01b5431361f0df5a275b21ab40be63083
Aviral Srivastava <aviralji4@gmail.com>
2022-01-07 22:21:44 +0530

    "updated the code base..."

--- new/0_p5test/test.py
+++ new/0_p5test/test.py
@@ -1,21 +1,7 @@
-from p5 import *
-
-def setup():
-    size(640, 360)
-    no_stroke()
-    background(204)
-
-def draw():
-    if mouse_is_pressed:
-        fill(random_uniform(255), random_uniform(127), random_uniform(51), 127)
+# function to return number of steps required in tower of hanoi
+def steps(n):
+    if n == 1:
+        return 1
     else:
-        fill(255, 15)
-
-    circle_size = random_uniform(low=10, high=80)
-
-    circle((mouse_x, mouse_y), circle_size)
-
-def key_pressed(event):
-    background(204)
-
-run()
+        return 2 * steps(n-1) + 1
+print(steps(3))
